79e0593565671041f22441043ec58f5766ae00b7
Artyom <itcoding76@gmail.com>
2023-03-27 21:30:11 +0200

    Task 1 is done

---
+++ J_HomeWork1/Main.java
@@ -0,0 +1,18 @@
+package J_HomeWork1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//============ Задача 1 ============
+        Task1 task1 = new Task1();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int number = sc.nextInt();
+        sc.close();
+
+        System.out.println(task1.sum(number));
+//============ Задача 2 ============
+    }
+}
