e3cd9c6c4d6a4dab0353824feff8fc0e7c27f835
ArtyomPV <118426357+ArtyomPV@users.noreply.github.com>
2023-03-29 14:35:09 +0200

    2я домашняя работа: задача №1 и №2 выполнена

--- homeWork2/Main.java
+++ homeWork2/Main.java
@@ -1,9 +1,27 @@
 package homeWork2;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+//        =========первая задача===================
+        StringBuilder sb = new StringBuilder();
+
+        ExampleHashMap data = new ExampleHashMap();
+        HashMap<String, String> keysAndValues = new HashMap<>();
+
+        keysAndValues.put("name", "Ivanov");
+        keysAndValues.put("country", "Russia");
+        keysAndValues.put("city", "Moscow");
+        keysAndValues.put("age", "null");
+
+        System.out.println(keysAndValues);
+        sb = data.getCondition(keysAndValues);
+        System.out.println(sb);
+
+
+
 //        =========вторая задача===================
         FileLog fileLog = new FileLog();
         int[] sortArr = {12, 6, 4, 1, 15, 10};
